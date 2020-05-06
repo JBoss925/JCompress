578f7b5a1900076555c995ba008987c328fd0b88
Jagger Brulato <jb2375@cornell.edu>
2020-05-06 18:27:14 -0400

    Compartmentalized code a bit more, moved all frequency operations to FrequencyCounter, now ready to implement the compression tree.

--- src/com/brulato/CompressorManager.java
+++ src/com/brulato/CompressorManager.java
@@ -11,39 +11,22 @@ public class CompressorManager {
     public static FileInputStream inputStream;
     public static FileOutputStream outputStream;
     public static int bytesPerLeaf;
-    public static byte[] bytesBuffer;
 
     public CompressorManager(FileInputStream inputStream, FileOutputStream outputStream, int bytesPerLeaf){
         CompressorManager.inputStream = inputStream;
         CompressorManager.outputStream = outputStream;
         CompressorManager.bytesPerLeaf = bytesPerLeaf;
-        CompressorManager.bytesBuffer = new byte[bytesPerLeaf];
     }
 
     public void prepareCompression(){
-        try{
-            while(true){
-                bytesBuffer = new byte[bytesPerLeaf];
-                int bytesRead = inputStream.read(bytesBuffer, 0, bytesBuffer.length);
-                if(bytesRead == bytesPerLeaf){
-                    FrequencyCounter.addByteFreq(bytesBuffer);
-                } else if (bytesRead != -1){
-                    FrequencyCounter.addByteFreq(Arrays.copyOfRange(bytesBuffer, 0, bytesRead));
-                } else {
-                    break;
-                }
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-            throw new Error("There was an exception during compression!");
-        }
+        FrequencyCounter.countFrequencies();
         System.out.println("Prepared for compression!");
     }
 
     public void doCompression(){
         System.out.println(FrequencyCounter.bytes.stream().map(bytes -> new String(bytes, StandardCharsets.UTF_8)).collect(Collectors.toList()));
         System.out.println(FrequencyCounter.freqs);
-        BitWriter.writeBits(0, true);
+        BitWriter.writeBits(5, true);
     }
 
     public void writeCompressedFile(){
--- src/com/brulato/FrequencyCounter.java
+++ src/com/brulato/FrequencyCounter.java
@@ -7,10 +7,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.brulato.CompressorManager.bytesPerLeaf;
+
 public class FrequencyCounter {
 
     public static ArrayList<byte[]> bytes = new ArrayList<>();
     public static ArrayList<Integer> freqs = new ArrayList<>();
+    public static byte[] bytesBuffer;
 
     public static void addByteFreq(byte[] bytesVal){
         int index = -1;
@@ -26,6 +29,25 @@ public class FrequencyCounter {
             System.out.println("Not Contained");
             bytes.add(bytesVal);
             freqs.add(1);
+        }
+    }
+
+    public static void countFrequencies(){
+        try{
+            while(true){
+                bytesBuffer = new byte[CompressorManager.bytesPerLeaf];
+                int bytesRead = CompressorManager.inputStream.read(bytesBuffer, 0, bytesBuffer.length);
+                if(bytesRead == CompressorManager.bytesPerLeaf){
+                    FrequencyCounter.addByteFreq(bytesBuffer);
+                } else if (bytesRead != -1){
+                    FrequencyCounter.addByteFreq(Arrays.copyOfRange(bytesBuffer, 0, bytesRead));
+                } else {
+                    break;
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new Error("There was an exception during compression!");
         }
     }
 
